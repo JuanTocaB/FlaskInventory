aef3d88459281ab57aa2c2f57a3163ee0d856cd5
JuanTocaB <juan.toca@alumnos.uneatlantico.es>
2025-05-18 21:25:20 +0200

    Add CORS support for GraphQL routes in Flask application

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.graphQL import graphqlRoutes
 
 app: Flask = Flask(__name__)
+CORS(app, resources={r"/graphql/*": {"origins": "*"}})
 app.register_blueprint(graphqlRoutes)
